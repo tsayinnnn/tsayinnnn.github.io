0551685441124e7e0b23b2d334b30c46e7a4756d
tsayinnnn <chalenejuanfernandez@gmail.com>
2025-07-21 10:17:14 +0800

    initial files

--- myPortfolio/vite.config.js
+++ myPortfolio/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 👇 Add this
+  base: "MyPortfolio/", // 👈 Change this to your repository name
   plugins: [react()],
 })
